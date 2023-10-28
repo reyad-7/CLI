0210cdc48085056fd6c164bd6dc7b98352fe581c
Moreyad <91405739+reyad-7@users.noreply.github.com>
2023-10-28 18:09:29 +0200

    add main libraries

--- javafile/src/Main.java
+++ javafile/src/Main.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.util.*;
+import java.nio.file.*;
+//import java.awt.event.TextEvent;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
